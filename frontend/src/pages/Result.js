e1b988959d492ec65a4df82be8f05e2288419f69
Elliott Coleman <ejaycoleman@googlemail.com>
2021-07-21 13:11:03 +0100

    select symbol from table

--- frontend/src/pages/Result.js
+++ frontend/src/pages/Result.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from 'react'
+import SymbolInformation from '../components/SymbolInformation'
 import { APIKeyContext } from '../context/APIKeyContext'
 
 import { searchSymbols } from '../services/SymbolSearch'
@@ -9,6 +10,7 @@ export default function Result () {
   const [filterValue, setFilterValue] = useState('')
 
   const [result, setResult] = useState([])
+  const [selected, setSelected] = useState('')
 
   const apiKeyContext = useContext(APIKeyContext)
 
@@ -23,7 +25,13 @@ export default function Result () {
       <input placeholder='filter term' value={filterTerm} onChange={e => setFilterTerm(e.target.value)}></input>
       <input placeholder='filter value' value={filterValue} onChange={e => setFilterValue(e.target.value)}></input>
       <button onClick={() => submit()}>Search</button>
-      {result.map(e => JSON.stringify(e))}
+
+      <table>
+        <tbody>
+          {result.map((e, i) => <tr key={i}><td>{e['2. name']}: {e['1. symbol']}</td><td><button onClick={() => setSelected(e['1. symbol'])}>Show More</button></td></tr>)}
+        </tbody>
+      </table>
+      <SymbolInformation symbol={selected} />
     </div>
   )
 }
