df5903a62d935201c29169ca543b828e00437dc3
Elliott Coleman <ejaycoleman@googlemail.com>
2021-07-25 12:38:12 +0100

    add second table for weekly data

--- frontend/src/components/SymbolInformation.js
+++ frontend/src/components/SymbolInformation.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useContext } from 'react'
 import PropTypes from 'prop-types'
 
-import { timeSeriesDaily, currentQuote } from '../services/SymbolSearch'
+import { timeSeriesDaily, currentQuote, timeSeriesWeekly } from '../services/SymbolSearch'
 
 import { APIKeyContext } from '../context/APIKeyContext'
 
@@ -12,6 +12,7 @@ import CardContent from '@material-ui/core/CardContent'
 
 export default function SymbolInformation ({ symbol }) {
   const [dailyInfo, setDailyInfo] = useState([])
+  const [weeklyInfo, setWeeklyInfo] = useState([])
   const [currentQuoteValue, setCurrentQuoteValue] = useState({})
 
   const apiKeyContext = useContext(APIKeyContext)
@@ -20,6 +21,9 @@ export default function SymbolInformation ({ symbol }) {
     if (symbol) {
       const dailyInfoResult = await timeSeriesDaily({ apiKey: apiKeyContext.apiKey, symbol: symbol['1. symbol'] })
       dailyInfoResult && setDailyInfo(dailyInfoResult)
+
+      const weeklyInfoResult = await timeSeriesWeekly({ apiKey: apiKeyContext.apiKey, symbol: symbol['1. symbol'] })
+      weeklyInfoResult && setWeeklyInfo(weeklyInfoResult)
 
       const getCurrentQuote = await currentQuote({ apiKey: apiKeyContext.apiKey, symbol: symbol['1. symbol'] })
       setCurrentQuoteValue(getCurrentQuote['Global Quote'])
@@ -79,6 +83,10 @@ export default function SymbolInformation ({ symbol }) {
       <h2>Historical prices (daily)</h2>
       <div style={{ height: 400, width: '100%' }}>
         {dailyInfo.length !== 0 && <DataGrid disableColumnResize={false} rows={[...dailyInfo].slice(1)} columns={generateDataGridColumns(dailyInfo[0])} pageSize={5} />}
+      </div>
+      <h2>Historical prices (weekly)</h2>
+      <div style={{ height: 400, width: '100%' }}>
+        {weeklyInfo.length !== 0 && <DataGrid disableColumnResize={false} rows={[...weeklyInfo].slice(1)} columns={generateDataGridColumns(weeklyInfo[0])} pageSize={5} />}
       </div>
     </div>
   )
